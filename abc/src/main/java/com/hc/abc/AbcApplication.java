4a1d0dfe0d221703d6ac5eca23ea8b46f0439fdd
C.HE <chuan.he@baozun.com>
2020-03-24 12:54:20 +0800

    11

--- abc/src/main/java/com/hc/abc/AbcApplication.java
+++ abc/src/main/java/com/hc/abc/AbcApplication.java
@@ -8,7 +8,7 @@ public class AbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AbcApplication.class, args);
-
+        System.out.println("1111");
     }
 
 }

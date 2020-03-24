40f6954e3ef549365716c71842c7adcc70a15225
C.HE <chuan.he@baozun.com>
2020-03-24 12:39:43 +0800

    init

---
+++ abc/src/test/java/com/hc/abc/AbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.abc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

11ceb6873ce6ddc78a3ede644167ee009f510d1d
C.HE <chuan.he@baozun.com>
2020-04-23 19:37:33 +0800

    Revert "111"
    This reverts commit 11e44b6ffc7eb0103d8f469074a5840a6eb535b5.
    
    # Conflicts:
    #	abc/src/main/java/com/hc/abc/AbcApplication.java

--- abc/src/main/java/com/hc/abc/AbcApplication.java
+++ abc/src/main/java/com/hc/abc/AbcApplication.java
@@ -8,8 +8,6 @@ public class AbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AbcApplication.class, args);
-        //11111
-        //22222
     }
 
 }

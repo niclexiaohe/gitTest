7a973b9f0300a58da6ad344310bad4cd8901c448
C.HE <chuan.he@baozun.com>
2020-04-24 09:02:54 +0800

    Revert "33"
    This reverts commit 2ccdcd43225957fc94a527deb2ef0b90c0490392.

--- abc/src/main/java/com/hc/abc/AbcApplication.java
+++ abc/src/main/java/com/hc/abc/AbcApplication.java
@@ -8,7 +8,6 @@ public class AbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AbcApplication.class, args);
-        //3333333333
         //1111111111
         //2222222222
     }
